f26618d961170e8b3c3c20a7538a141d93624d54
Najam <realnajmiter@gmail.com>
2026-07-12 01:54:02 +0500

    oh ma phyiscs 🥹

---
+++ src/constants.zig
@@ -0,0 +1,8 @@
+/// Scroll speed.
+pub const SCROLL_SPEED = 2.5;
+/// Scroll lerp amount.
+pub const SCROLL_LERP = 0.05;
+/// Scroll friction
+pub const FRICTION = 0.8;
+/// Spring ka chichra-pan (meysa-pan in other words)
+pub const STIFFNESS = 0.08;
--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const rl = @import("raylib");
 const cl = @import("colors.zig");
+const c = @import("constants.zig");
 
 const math = rl.math;
 
@@ -9,12 +10,23 @@ pub fn main() !void {
     rl.setTargetFPS(60);
 
     var camera = rl.Camera2D{ .target = rl.Vector2{ .x = 0, .y = 0 }, .offset = rl.Vector2{ .x = 0, .y = 0 }, .rotation = 0, .zoom = 1 };
+    var scrollY: f32 = 0.0;
+    var velocityY: f32 = 0.0;
 
     while (!rl.windowShouldClose()) {
         const wheel = rl.getMouseWheelMove();
 
-        camera.target.y -= wheel * 10;
-        camera.target.y = math.clamp(camera.target.y, 0, 900);
+        scrollY -= wheel * c.SCROLL_SPEED;
+        scrollY = math.clamp(scrollY, 0, 900);
+
+        const distance = camera.target.y - scrollY;
+        const springForce = -c.STIFFNESS * distance; // somewhere in here is hooke's law: (F = -kx) they never told us it was this beautiful 🥹
+        const stopingForce = -c.FRICTION * velocityY;
+
+        velocityY += springForce + stopingForce;
+        camera.target.y += velocityY;
+
+        camera.target.y = math.lerp(camera.target.y, scrollY, c.SCROLL_LERP);
 
         rl.beginDrawing();
         rl.clearBackground(cl.BLACK);
